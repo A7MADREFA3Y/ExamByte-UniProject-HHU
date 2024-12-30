17daf8e9d4598efd2b20edac2ee45d3a62b3f636
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2024-12-30 09:01:31 +0100

    added answer model

---
+++ src/main/java/org/example/exambyte/model/Answer.java
@@ -0,0 +1,30 @@
+package org.example.exambyte.model;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "answers")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @Column(name = "answer_text", nullable = false)
+    private String answerText;
+
+    @ManyToOne
+    @JoinColumn(name = "test_result_id", nullable = false)
+    private TestResult testResult;
+}
