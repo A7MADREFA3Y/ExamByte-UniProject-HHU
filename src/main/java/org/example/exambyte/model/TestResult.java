000c77f4374c26ea14ff7b17becbea48f3f2216e
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2024-12-30 09:09:20 +0100

    added TestResult model for the feedBack

---
+++ src/main/java/org/example/exambyte/model/TestResult.java
@@ -0,0 +1,45 @@
+package org.example.exambyte.model;
+
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "test_results")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "test_id", nullable = false)
+    private Test test;
+
+    @ManyToOne
+    @JoinColumn(name = "taken_by", nullable = false)
+    private User takenBy;
+
+    @OneToMany(mappedBy = "testResult", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Answer> answers;
+
+    @Column(name = "submit_date", nullable = false)
+    private LocalDateTime submitDate;
+
+    @Column(name = "grade")
+    private Double grade;
+
+    @Column(name = "passed")
+    private Boolean passed;
+
+    @Column(name = "corrected_by")
+    private String correctedBy;
+}
