35d9543df01f0ef1cee372bb7c092e3faf60db70
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2025-01-03 00:12:11 +0100

    added createTestForm GetMapping and SaveTest PostMapping

--- src/main/java/org/example/exambyte/webConroller/AdminController.java
+++ src/main/java/org/example/exambyte/webConroller/AdminController.java
@@ -6,8 +6,6 @@ import org.example.exambyte.model.Test;
 import org.springframework.security.core.Authentication;
 import org.example.exambyte.service.ServiceInterface;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,8 +15,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/adminDashBoard")
@@ -56,12 +52,11 @@ public class AdminController {
 
     @PostMapping("/newTest")
     public String SaveTest(@ModelAttribute("test") TestsDto testsDto,
-                           Model model, BindingResult bindingResult) {
+                           BindingResult bindingResult) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
         OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
-        String githubUsername = (String) oauth2User.getAttributes().get("login"); // GitHub username
+        String githubUsername = (String) oauth2User.getAttributes().get("login");
 
         // Set the GitHub username to 'createdBy'
         testsDto.setCreatedBy(githubUsername);
