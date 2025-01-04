d4f7ff5fb250dc6a9fe5a679a432aa3f99bfaeae
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2025-01-04 15:29:56 +0100

    added validation to the create test NotNull and NotEmpty

--- src/main/java/org/example/exambyte/dto/TestsDto.java
+++ src/main/java/org/example/exambyte/dto/TestsDto.java
@@ -1,20 +1,43 @@
 package org.example.exambyte.dto;
 
-
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.example.exambyte.model.Question;
+import org.example.exambyte.model.TestResult;
+
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 @Data
 @Builder
+@Valid
 public class TestsDto {
+
     private Long id;
+
+    @NotNull
+    @NotEmpty(message = "please Insert Test Name")
     private String testName;
+
     private String createdBy;
+
+    @NotNull(message = "please Insert starting time")
     private LocalDateTime startTime;
+
+    @NotNull(message = "please Insert deadline time")
     private LocalDateTime endTime;
+
+    @NotNull(message = "Result publication time is required")
     private LocalDateTime resultPublicationTime;
+
+    private List<Question> questions;
+    private List<TestResult> results;
+
 
 }
--- src/main/java/org/example/exambyte/webConroller/AdminController.java
+++ src/main/java/org/example/exambyte/webConroller/AdminController.java
@@ -1,6 +1,7 @@
 package org.example.exambyte.webConroller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.example.exambyte.dto.TestsDto;
 import org.example.exambyte.model.Test;
 import org.springframework.security.core.Authentication;
@@ -10,10 +11,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -34,39 +32,37 @@ public class AdminController {
         if(!service.checkIfAdmin(auth)){
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         }
-
         model.addAttribute("tests", service.getAllTests());
-
         return "AdminTemp/adminDash";
     }
 
 
-
     @GetMapping("/newTest")
-    public String createTestForm(Model model) {
-
+    public String createTestForm( Model model) {
         Test test = new Test();
         model.addAttribute("test", test);
         return "AdminTemp/create-Test";
     }
 
     @PostMapping("/newTest")
-    public String SaveTest(@ModelAttribute("test") TestsDto testsDto,
+    public String SaveTest(@Valid @ModelAttribute("test") TestsDto testsDto,
+                           Model model,
                            BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("test", testsDto);
+            return "AdminTemp/create-Test";
+        }
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
         String githubUsername = (String) oauth2User.getAttributes().get("login");
+        if (githubUsername == null) {
+            throw new IllegalStateException("GitHub username is not available");
+        }
 
-        // Set the GitHub username to 'createdBy'
+        //to set as default how created this test
         testsDto.setCreatedBy(githubUsername);
-
-//        if(bindingResult.hasErrors()) {
-//            System.out.println("2");
-//
-//            model.addAttribute("test", testsDto);
-//            return "AdminTemp/create-Test";
-//        }
 
         service.saveTest(testsDto);
         return "redirect:/adminDashBoard/";
