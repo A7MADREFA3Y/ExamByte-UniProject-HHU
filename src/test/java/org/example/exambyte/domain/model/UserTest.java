b5fe33621cb251a66dc8d217996573ae95909815
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2025-01-14 19:08:16 +0100

    added tests for users

---
+++ src/test/java/org/example/exambyte/domain/model/UserTest.java
@@ -0,0 +1,68 @@
+package org.example.exambyte.domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.example.exambyte.domain.model.Role.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    @DisplayName("User should have an Id")
+    void userShouldHaveGithub_id(){
+        User user = new User();
+        user.setGithubId("123456");
+        assertEquals("123456", user.getGithubId());
+    }
+
+    @Test
+    @DisplayName("User should have an github username")
+    void userShouldHaveGithub_username(){
+        User user = new User();
+        user.setGithubUsername("refai");
+        assertEquals("refai", user.getGithubUsername());
+    }
+
+    @Test
+    @DisplayName("User can have an Role as USER")
+    void userCanHaveRole_User(){
+        User user = new User();
+        user.setRole(USER);
+        assertEquals(USER, user.getRole());
+    }
+
+    @Test
+    @DisplayName("User can have an Role as ADMIN")
+    void userCanHaveRole_ADMIN(){
+        User user = new User();
+        user.setRole(ADMIN);
+        assertEquals(ADMIN, user.getRole());
+    }
+
+    @Test
+    @DisplayName("User can have an Role as CORRECTOR")
+    void userCanHaveRole_CORRECTOR(){
+        User user = new User();
+        user.setRole(CORRECTOR);
+        assertEquals(CORRECTOR, user.getRole());
+    }
+
+    @Test
+    @DisplayName("User can have all attributes Github_Id, Github_username and role")
+    void userCanHaveAllThreeAttributes(){
+        User user = new User();
+
+        user.setGithubId("123456");
+        user.setGithubUsername("refai");
+        user.setRole(ADMIN);
+
+        assertEquals("123456", user.getGithubId());
+        assertEquals("refai", user.getGithubUsername());
+        assertEquals(ADMIN, user.getRole());
+    }
+
+
+
+}
