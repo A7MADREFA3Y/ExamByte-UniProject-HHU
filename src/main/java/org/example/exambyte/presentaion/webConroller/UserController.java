c4d971a1f8d2a88b4f720ac8b333615d2a71bb63
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2025-01-18 15:02:03 +0100

    adjust the usercontroller to take the post and get mapping from taking and submitting the test

--- src/main/java/org/example/exambyte/presentaion/webConroller/UserController.java
+++ src/main/java/org/example/exambyte/presentaion/webConroller/UserController.java
@@ -1,15 +1,20 @@
 package org.example.exambyte.presentaion.webConroller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.exambyte.application.dto.AnswerDto;
+import org.example.exambyte.application.dto.AnswersDto;
 import org.example.exambyte.application.service.serviceQuestion.ServiceQuestionInterface;
 import org.example.exambyte.application.service.serviceTest.ServiceImp;
 import org.example.exambyte.application.service.serviceTest.ServiceInterface;
+import org.example.exambyte.domain.model.Answer;
+import org.example.exambyte.domain.model.Question;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -41,12 +46,34 @@ public class UserController {
 
 
     @GetMapping("/{testId}/Start")
-    public String StartTest(Model model, @PathVariable("testId") Long testId) {
-
+    public String startTest(Model model, @PathVariable("testId") Long testId) {
+        model.addAttribute("questions", serviceQuestion.getAllQuestionByTestId(testId));
         model.addAttribute("username", service.getGithubUsername());
         model.addAttribute("test", service.findTestById(testId));
-        model.addAttribute("questions", serviceQuestion.getAllQuestionByTestId(testId));
 
+        AnswersDto answersDto = new AnswersDto();
+        List<Question> questions = serviceQuestion.getAllQuestionByTestId(testId);
+        for (Question question : questions) {
+            AnswerDto answerDto = new AnswerDto();
+            answerDto.setQuestionId(question.getId());
+            answersDto.getAnswers().add(answerDto);
+        }
+
+        model.addAttribute("answers", answersDto);
         return "UserTemp/take-test";
+    }
+
+
+    @PostMapping("/{testId}/Start")
+    public String submitTest(@ModelAttribute("answers") AnswersDto answersDto
+    , @PathVariable("testId") Long testId) {
+
+        for (AnswerDto answer : answersDto.getAnswers()) {
+            answer.setTestId(testId);
+            answer.setTakenBy(service.getGithubUsername());
+            service.saveAnswer(answer);
+        }
+
+        return "redirect:/userDashBoard/";
     }
 }
