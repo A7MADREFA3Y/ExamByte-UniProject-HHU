6b5a1306896394495c907229ce87a960f360db2a
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2025-01-19 13:40:33 +0100

    added V6 sql

--- src/main/java/org/example/exambyte/application/dto/TestResultDto.java
+++ src/main/java/org/example/exambyte/application/dto/TestResultDto.java
@@ -1,4 +1,24 @@
 package org.example.exambyte.application.dto;
 
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
 public class TestResultDto {
+
+    private Long id;
+    private Long testId;
+    private String takenBy;
+    private List<AnswerDto> answers;
+    private LocalDateTime submitDate;
+    private Double score;
+    private Boolean passed;
+    private Boolean graded;
+    private String correctedBy;
+
 }
