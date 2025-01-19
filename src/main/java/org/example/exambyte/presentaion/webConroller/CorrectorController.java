797aaad2906ab2a50d561e7753d8c3dd54e1c54b
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2025-01-19 02:45:31 +0100

    added the user model view to controller

--- src/main/java/org/example/exambyte/presentaion/webConroller/CorrectorController.java
+++ src/main/java/org/example/exambyte/presentaion/webConroller/CorrectorController.java
@@ -2,8 +2,10 @@ package org.example.exambyte.presentaion.webConroller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.exambyte.application.service.serviceTest.ServiceImp;
+import org.example.exambyte.application.service.serviceTest.ServiceInterface;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,17 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/correctorDashBoard")
 public class CorrectorController {
 
-    private ServiceImp service;
+    private final ServiceInterface service;
 
-    public CorrectorController(ServiceImp service) {
+    public CorrectorController(ServiceInterface service) {
         this.service = service;
     }
 
     @GetMapping("/")
-    public String DashBoardCorrector(Authentication auth, HttpServletResponse response) {
+    public String DashBoardCorrector(Authentication auth, HttpServletResponse response, Model model) {
         if(!(service.checkIfCorrector(auth) || (service.checkIfAdmin(auth)))) {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         }
+        model.addAttribute("username", service.getGithubUsername());
         return "CorrectorTemp/correctorDash";
     }
 }
