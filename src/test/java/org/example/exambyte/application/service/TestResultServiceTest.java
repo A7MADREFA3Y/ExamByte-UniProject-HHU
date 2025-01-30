231b12b1e663684dfa101ca75f1cd42e3a225793
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2025-01-30 16:07:03 +0100

    added tests for TestResultServiceTest for all methods except getGithubusername

--- src/test/java/org/example/exambyte/application/service/TestResultServiceTest.java
+++ src/test/java/org/example/exambyte/application/service/TestResultServiceTest.java
@@ -8,12 +8,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -78,6 +78,58 @@ public class TestResultServiceTest {
         assertThat(allTestResultsWithTestIdAndUsername).isEqualTo(testResultsList);
     }
 
+    
+    @Test
+    @DisplayName("getTestResultWithTestIdAndUsername the methode get only one spicific TestResult")
+    void testGetTestResultWithTestIdAndUsername() {
+        Long testId = 1L;
+        String username = "ahmad";
 
+
+        TestResult testResult = TestResult.builder()
+                .id(2L)
+                .testId(testId)
+                .takenBy(username)
+                .submitDate(LocalDateTime.now())
+                .grade(15.0)
+                .graded(false)
+                .passed(false)
+                .build();
+
+        when(testResultRepo.findTestResultByTestIdAndUsername(testId, username)).thenReturn(testResult);
+
+        TestResult testResultWithTestIdAndUsername = testResultService.getTestResultWithTestIdAndUsername(testId, username);
+
+        assertThat(testResultWithTestIdAndUsername).isEqualTo(testResult);
+
+    }
+
+    @Test
+    @DisplayName("updateTestResult update the Test Result From the corrector")
+    void testUpdateTestResult() {
+
+
+        TestResultRepository mockTestResultRepo = Mockito.mock(TestResultRepository.class);
+
+        TestResultServiceImp testResultServiceImp = Mockito.spy(new TestResultServiceImp(mockTestResultRepo));
+
+        doReturn("refai").when(testResultServiceImp).getGithubUsername();
+
+        TestResult testResult = TestResult.builder()
+                .id(155L)
+                .testId(123L)
+                .takenBy("ahmad")
+                .submitDate(LocalDateTime.now())
+                .grade(15.0)
+                .graded(false)
+                .build();
+
+        testResultServiceImp.updateTestResult(testResult);
+
+        testResultRepo.saveTestResult(testResult);
+
+        verify(testResultRepo,times(1)).saveTestResult(testResult);
+
+    }
 
 }
