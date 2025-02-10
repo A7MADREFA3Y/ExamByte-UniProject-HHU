8aea0967dcadb64e2da424d131b6b9e448b08c4e
A7MADREFA3Y <xen96day@uni-duesseldorf.de>
2025-02-10 03:30:12 +0100

    added the documentation to the ServiceQuestionsImp

--- src/main/java/org/example/exambyte/application/service/serviceQuestion/ServiceQuestionsImp.java
+++ src/main/java/org/example/exambyte/application/service/serviceQuestion/ServiceQuestionsImp.java
@@ -17,33 +17,27 @@ public class ServiceQuestionsImp implements ServiceQuestionInterface{
         this.questionRepository = questionRepository;
     }
 
+    /***
+     *
+     * @param id takes the Question ID to search for specific Question
+     * @return the Question that have that ID
+     */
+
     @Override
     public Question findQuestionById(Long id) {
         return questionRepository.findById(id);
     }
+
+    /***
+     *
+     * @param questionDto takes Dto and map it to Entity using mapToQuestion methode then save it
+     */
 
     @Override
     public void saveQuestion(QuestionDto questionDto) {
         Question question = mapToQuestion(questionDto);
         questionRepository.save(question);
     }
-
-    @Override
-    public List<Question> getAllQuestionByTestId(Long testId) {
-        return questionRepository.findByTestId(testId);
-    }
-
-    @Override
-    public List<Question> getAllQuestionByTestIdAndHaveTypeAsFREE_TEXT(Long testId) {
-        return questionRepository.findByTestIdAndHaveTypeAsFREE_TEXT(testId);
-
-    }
-
-    @Override
-    public void deleteQuestionById(Long questionId) {
-        questionRepository.deleteQuestionById(questionId);
-    }
-
 
     private Question mapToQuestion(QuestionDto questionDto) {
         return Question.builder()
@@ -58,4 +52,41 @@ public class ServiceQuestionsImp implements ServiceQuestionInterface{
                 .correctAnswer(questionDto.getCorrectAnswer())
                 .build();
     }
+
+    /***
+     *
+     * @param testId takes the Test ID to search for all the Question that have the same Test ID
+     * @return a List of Questions
+     */
+
+    @Override
+    public List<Question> getAllQuestionByTestId(Long testId) {
+        return questionRepository.findByTestId(testId);
+    }
+
+    /***
+     *
+     * @param testId takes the Test ID to search for all the Question that have the same Test ID,
+     *               and they have Type as FREE TEXT
+     * @return a List of Questions that are Free Text
+     */
+
+    @Override
+    public List<Question> getAllQuestionByTestIdAndHaveTypeAsFREE_TEXT(Long testId) {
+        return questionRepository.findByTestIdAndHaveTypeAsFREE_TEXT(testId);
+
+    }
+
+    /***
+     *
+     * @param questionId takes the Question ID to Delete the Question
+     */
+
+    @Override
+    public void deleteQuestionById(Long questionId) {
+        questionRepository.deleteQuestionById(questionId);
+    }
+
+
+
 }
